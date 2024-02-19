bb6fbca67d59e8c266620e190629fe1ba3b4b6b6
Pedro Victor Somensi <psomensi.contato@gmail.com>
2024-02-19 20:24:26 -0300

    first commit

---
+++ asyncawait-example/asyncawait_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  asyncawait_exampleApp.swift
+//  asyncawait-example
+//
+//  Created by Pedro Somensi on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct asyncawait_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
